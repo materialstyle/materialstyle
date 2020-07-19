c8a35d4d00fcf1d67955ce3b4a95a75334d81585
Neeraj Kumar Das <nkdas91@gmail.com>
2020-07-19 22:04:03 +0530

    Wrapped outlined text field label with divs to remove the requirement of setting background color on label

--- src/js/text_field.js
+++ src/js/text_field.js
@@ -27,9 +27,6 @@ class TextField {
 
         this._iconLeft = element.querySelector('.icon-left')
         this._iconRight = element.querySelector('.icon-right')
-
-        this._paddingLeft = this._iconLeft != null ? this._iconLeft.offsetWidth : 0
-        this._paddingRight = this._iconRight != null ? this._iconRight.offsetWidth : 0
     }
 
     static get VERSION() {
@@ -45,10 +42,16 @@ class TextField {
                 data = new TextField(this)
                 $element.data(DATA_KEY, data)
 
+                data['initTextFields']()
+                data['initLabel']()
                 data['changeLabelColor']()
                 data['changeLabelPosition']()
                 data['changeTextFieldColor']()
                 data['initInputGroup']()
+
+                if (data._inputFieldClass == CLASS_NAME_TEXTFIELD_OUTLINE) {
+                    data._notch.style.height = data._inputField.offsetHeight + 'px';
+                }
 
                 $(data._inputField).on('change', function () {
                     data._inputValueLength = data._inputField.value.length
@@ -73,11 +76,70 @@ class TextField {
         })
     }
 
+    initTextFields() {
+        if (this._inputFieldClass == CLASS_NAME_TEXTFIELD) {
+            let ripple = document.createElement('div');
+            ripple.className = 'ms-line-ripple';
+
+            this._ripple = ripple;
+            this._inputField.after(ripple);
+        }
+    }
+
+    initLabel() {
+        if (this._inputFieldClass == CLASS_NAME_TEXTFIELD_OUTLINE) {
+            let notch = document.createElement('div');
+            notch.className = 'ms-notch';
+
+            let notchBefore = document.createElement('div');
+            notchBefore.className = 'ms-notch-before';
+            notchBefore.style.borderColor = this._primaryColor;
+
+            let notchLabel = document.createElement('div');
+            notchLabel.className = 'ms-notch-label width-auto';
+            notchLabel.style.borderColor = this._primaryColor;
+            notchLabel.style.width = ((this._inputLabel.offsetWidth * 0.75) + 10) + 'px';
+            if (this._inputLabelClass == CLASS_NAME_STATIC_LABEL) {
+                notchLabel.style.borderTopWidth = 0;
+            }
+
+            let notchAfter = document.createElement('div');
+            notchAfter.className = 'ms-notch-after';
+            notchAfter.style.borderColor = this._primaryColor;
+
+            // Wrap notchLabel around label
+            this._inputLabel.parentNode.insertBefore(notchLabel, this._inputLabel);
+            notchLabel.appendChild(this._inputLabel);
+
+            // Wrap notch around notchBefore, notchLabel and notchAfter
+            notchLabel.parentNode.insertBefore(notchBefore, notchLabel);
+            notchLabel.parentNode.insertBefore(notchAfter, notchLabel);
+            notchLabel.parentNode.insertBefore(notch, notchLabel);
+
+            notch.appendChild(notchBefore);
+            notch.appendChild(notchLabel);
+            notch.appendChild(notchAfter);
+
+            this._notch = notch;
+            this._notchBefore = notchBefore;
+            this._notchLabel = notchLabel;
+            this._notchAfter = notchAfter;
+        }
+    }
+
     changeLabelColor() {
         if (this._inputValueLength) {
             this._inputLabel.style.color = this._accentColor;
+            if (this._inputFieldClass === CLASS_NAME_TEXTFIELD_OUTLINE) {
+                this._notchLabel.style.borderTopWidth = 0;
+            }
         } else {
             this._inputLabel.style.color = this._primaryColor;
+            if (this._inputFieldClass === CLASS_NAME_TEXTFIELD_OUTLINE
+                && this._inputLabelClass == CLASS_NAME_FLOATING_LABEL
+            ) {
+                this._notchLabel.style.borderTopWidth = '1px';
+            }
         }
     }
 
@@ -95,7 +157,7 @@ class TextField {
 
     changeTextFieldColor() {
         if (this._inputFieldClass === CLASS_NAME_TEXTFIELD) {
-            this._inputField.style.backgroundImage =
+            this._ripple.style.backgroundImage =
                 'linear-gradient(' + this._accentColor + ', ' + this._accentColor + '), ' +
                 'linear-gradient(' + this._primaryColor + ', ' + this._primaryColor + ')';
         } else {
@@ -111,6 +173,11 @@ class TextField {
         if (this._inputFieldClass === CLASS_NAME_TEXTFIELD_OUTLINE) {
             this._inputField.style.borderColor = this._accentColor;
             this._inputField.style.boxShadow = 'inset 0 0 1px 1px ' + this._accentColor;
+            this._notchLabel.style.borderTopWidth = 0;
+            this._notch.classList.add('notch-active');
+            this._notchBefore.style.borderColor = this._accentColor;
+            this._notchLabel.style.borderColor = this._accentColor;
+            this._notchAfter.style.borderColor = this._accentColor;
         }
     };
 
@@ -121,20 +188,20 @@ class TextField {
         if (this._inputFieldClass === CLASS_NAME_TEXTFIELD_OUTLINE) {
             this._inputField.style.borderColor = this._primaryColor;
             this._inputField.style.boxShadow = 'none';
+            this._notch.classList.remove('notch-active');
+            this._notchBefore.style.borderColor = this._primaryColor;
+            this._notchLabel.style.borderColor = this._primaryColor;
+            this._notchAfter.style.borderColor = this._primaryColor;
         }
     }
 
     initInputGroup() {
-        if (0 !== this._paddingLeft) {
-            this._inputField.style.paddingLeft = this._paddingLeft + 'px';
-
-            if (this._inputLabel.className.includes(CLASS_NAME_FLOATING_LABEL)) {
-                this._inputLabel.style.transform = 'translate(' + this._paddingLeft + 'px, 30px)'
-            }
+        if (this._iconLeft != null) {
+            this._iconLeft.style.height = this._inputField.offsetHeight + 'px';
         }
 
-        if (0 !== this._paddingRight) {
-            this._inputField.style.paddingRight = this._paddingRight + 'px';
+        if (this._iconRight != null) {
+            this._iconRight.style.height = this._inputField.offsetHeight + 'px';
         }
     }
 }
