33e1328da0ee8294884fb3c475c43da5c6616f16
Neeraj Kumar Das <nkdas91@gmail.com>
2020-08-30 15:34:30 +0530

    Added sidebar and autohide navbar

---
+++ src/js/navbar.js
@@ -0,0 +1,42 @@
+/**
+ * --------------------------------------------------------------------------
+ * Material Style (v2.0.0): navbar.js
+ * Licensed under MIT (https://github.com/materialstyle/materialstyle/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+function setMsContainerPadding() {
+    let fixedTop = document.querySelector('.fixed-top')
+
+    if (fixedTop != null) {
+
+        let msContainer = document.querySelector('.ms-container')
+        if (msContainer != null) {
+            msContainer.style.paddingTop = (fixedTop.offsetHeight + 15) + 'px';
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    setMsContainerPadding();
+
+    window.addEventListener('resize', function () {
+        setMsContainerPadding();
+    });
+
+    let prevScrollpos = window.pageYOffset;
+
+    window.addEventListener('scroll', function () {
+        let currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            if (document.querySelector('.navbar.auto-hide')) {
+                document.querySelector('.navbar.auto-hide').classList.remove('hide');
+            }
+        } else {
+            if (document.querySelector('.navbar.auto-hide')) {
+                document.querySelector('.navbar.auto-hide').classList.add('hide');
+            }
+        }
+        prevScrollpos = currentScrollPos;
+    });
+});
