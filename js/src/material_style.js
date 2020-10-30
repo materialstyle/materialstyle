4a547a24cc2ee5bacecf4bdd93cbfd97b99447f8
Neeraj Kumar Das <nkdas91@gmail.com>
2020-10-30 17:36:02 +0530

    Sorted imports and exports

--- js/src/material_style.js
+++ js/src/material_style.js
@@ -1,29 +1,29 @@
 import '../../scss/material_style.scss'
 
+import './utility'
+import './navbar'
+
 import Alert from './alert'
 import Button from './button'
 import Carousel from './carousel'
 import Collapse from './collapse'
+import Drawer from './drawer'
 import Dropdown from './dropdown'
+import FileInput from './file_input'
 import Modal from './modal'
 import Popover from './popover'
-import Scrollspy from './scrollspy'
-import Tab from './tab'
-import Toast from './toast'
-import Tooltip from './tooltip'
-import Util from './util'
-
-import './utility'
-import './navbar'
-import Drawer from './drawer'
-import FileInput from './file_input'
+import Rainbow from './rainbow'
 import Ripple from './ripple'
+import Scrollspy from './scrollspy'
 import SelectField from './select_field'
 import Shape from './shape'
 import Snackbar from './snackbar'
 import Spinner from './spinner'
-import Rainbow from './rainbow'
+import Tab from './tab'
 import TextField from './text_field'
+import Toast from './toast'
+import Tooltip from './tooltip'
+import Util from './util'
 
 export default {
   Util,
@@ -31,20 +31,20 @@ export default {
   Button,
   Carousel,
   Collapse,
+  Drawer,
   Dropdown,
+  FileInput,
   Modal,
   Popover,
-  Scrollspy,
-  Tab,
-  Toast,
-  Tooltip,
-  Drawer,
-  FileInput,
+  Rainbow,
   Ripple,
+  Scrollspy,
   SelectField,
   Shape,
   Snackbar,
   Spinner,
-  Rainbow,
-  TextField
+  Tab,
+  TextField,
+  Toast,
+  Tooltip
 }
