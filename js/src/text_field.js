16ac7a7de3b71697766ebfd9bbb84a753d5c8db8
Neeraj Kumar Das <nkdas91@gmail.com>
2021-12-18 19:07:49 +0530

    Make .input-group display block Change .input-group-text position to absolute Add left and right padding to .form-control based on .input-group-text width Refactor code

--- js/src/text_field.js
+++ js/src/text_field.js
@@ -23,10 +23,8 @@ const NAME = 'textfield'
 const VERSION = '3.0.0-alpha1'
 
 const CLASS_NAME_TEXTFIELD = 'form-control'
-const CLASS_NAME_TEXTFIELD_OUTLINED = 'form-floating--outlined'
-const CLASS_NAME_INPUTGROUP_OUTLINED = 'input-group--outlined'
+const CLASS_NAME_OUTLINED = 'form-floating--outlined'
 
-const NOTCH_BEFORE_WIDTH = '.75rem'
 const LABEL_SCALE = 0.85
 
 class TextField extends BaseComponent {
@@ -73,30 +71,13 @@ class TextField extends BaseComponent {
     if (this._inputGroup) {
       this._prepend = this._inputGroup.querySelector('.prepend')
       this._append = this._inputGroup.querySelector('.append')
-
-      if (this._prepend) {
-        this._textField.style.paddingLeft = 0
-        this._label.style.paddingLeft = 0
-        this._textField.style.borderTopLeftRadius = 0
-      } else {
-        this._textField.style.borderTopLeftRadius = '.25rem'
-      }
-
-      if (this._append) {
-        this._textField.style.paddingRight = 0
-        this._label.style.paddingRight = 0
-        this._textField.style.borderTopRightRadius = 0
-      } else {
-        this._textField.style.borderTopRightRadius = '.25rem'
-      }
-
-      this._inputGroup.addEventListener('click', () => this._textField.focus())
     }
 
-    if (this._element.className.includes(CLASS_NAME_TEXTFIELD_OUTLINED)) {
-      if (this._inputGroup) {
-        this._inputGroup.classList.add(CLASS_NAME_INPUTGROUP_OUTLINED)
-      }
+    if (this._prepend) {
+      this._label.style.paddingLeft = 0
+    }
+
+    if (this._element.className.includes(CLASS_NAME_OUTLINED)) {
       this.addNotch()
     } else {
       this.addRipple()
@@ -146,19 +127,11 @@ class TextField extends BaseComponent {
       if (this._inputGroup) {
         if (this._prepend) {
           this._prepend.style.height = `${this._textField.offsetHeight}px`
-          this._element.style.setProperty('--prepend-width', `-${this._prepend.offsetWidth}px`)
-          this._element.style.setProperty('--label-translate-x', `calc(${this._prepend.offsetWidth}px - ${NOTCH_BEFORE_WIDTH})`)
-
-          if (this._element.querySelector('.valid-feedback')) {
-            this._element.querySelector('.valid-feedback').style.transform = `translateX(-${this._prepend.offsetWidth}px)`
-          }
-          if (this._element.querySelector('.invalid-feedback')) {
-            this._element.querySelector('.invalid-feedback').style.transform = `translateX(-${this._prepend.offsetWidth}px)`
-          }
+          this._element.style.setProperty('--prepend-width', `${this._prepend.offsetWidth}px`)
         }
         if (this._append) {
           this._append.style.height = `${this._textField.offsetHeight}px`
-          this._element.style.setProperty('--append-width', `-${this._append.offsetWidth}px`)
+          this._element.style.setProperty('--append-width', `${this._append.offsetWidth}px`)
         }
       }
     })
