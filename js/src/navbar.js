b8a26f55eb7de6ab86f3b6332742f606bb3504e9
Neeraj Kumar Das <nkdas91@gmail.com>
2022-01-02 14:59:48 +0530

    Set ```--navbar-fixed-height``` custom property to body Create a class NavbarFixed for ```.navbar.fixed-*.auto-hide``` related JS Update example

--- js/src/navbar.js
+++ js/src/navbar.js
@@ -5,35 +5,47 @@
  * --------------------------------------------------------------------------
  */
 
-function setMsContainerPadding() {
-  const fixedTop = document.querySelector('.fixed-top')
+class NavbarFixed {
+  constructor() {
+    this.addEventListeners()
+  }
 
-  if (fixedTop !== null) {
-    const mRoot = document.querySelector('.m-root')
-    if (mRoot !== null) {
-      mRoot.style.setProperty('--navbar-fixed-top-height', `${fixedTop.offsetHeight}px`)
+  setNavbarFixedHeight() {
+    const navbarFixed = document.querySelector('.navbar[class*="fixed-"]')
+
+    if (navbarFixed !== null) {
+      document.body.style.setProperty('--navbar-fixed-height', `${navbarFixed.offsetHeight}px`)
     }
+  }
+
+  addEventListeners() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.setNavbarFixedHeight()
+    })
+
+    window.addEventListener('resize', () => {
+      this.setNavbarFixedHeight()
+    }, {
+      passive: true
+    })
+
+    let prevScrollpos = window.pageYOffset
+    window.addEventListener('scroll', () => {
+      const currentScrollPos = window.pageYOffset
+
+      if (prevScrollpos > currentScrollPos) {
+        if (document.querySelector('.navbar.auto-hide')) {
+          document.querySelector('.navbar.auto-hide').classList.remove('hide')
+        }
+      } else if (document.querySelector('.navbar.auto-hide')) {
+        document.querySelector('.navbar.auto-hide').classList.add('hide')
+      }
+
+      prevScrollpos = currentScrollPos
+    }, {
+      passive: true
+    })
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  setMsContainerPadding()
-
-  window.addEventListener('resize', () => {
-    setMsContainerPadding()
-  })
-
-  let prevScrollpos = window.pageYOffset
-
-  window.addEventListener('scroll', () => {
-    const currentScrollPos = window.pageYOffset
-    if (prevScrollpos > currentScrollPos) {
-      if (document.querySelector('.navbar.auto-hide')) {
-        document.querySelector('.navbar.auto-hide').classList.remove('hide')
-      }
-    } else if (document.querySelector('.navbar.auto-hide')) {
-      document.querySelector('.navbar.auto-hide').classList.add('hide')
-    }
-    prevScrollpos = currentScrollPos
-  })
-})
+(() => new NavbarFixed())()
