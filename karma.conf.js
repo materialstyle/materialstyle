1c26e94545474c7f21fa96f92e0241b73d817b09
Neeraj Kumar Das <nkdas91@gmail.com>
2022-01-19 20:47:26 +0530

    Add BrowserStack karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,21 @@ const path = require('path')
 const {browsers} = require('./browsers')
 
 const ENV = process.env
+const BROWSERSTACK = Boolean(ENV.BROWSERSTACK)
 const DEBUG = Boolean(ENV.DEBUG)
+
+const frameworks = [
+  'jasmine',
+  'webpack'
+]
+
+const plugins = [
+  'karma-jasmine',
+  'karma-webpack',
+  'karma-sourcemap-loader'
+]
+
+const reporters = ['progress']
 
 const detectBrowsers = {
   usePhantomJS: false,
@@ -30,68 +44,93 @@ const detectBrowsers = {
   }
 }
 
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    port: 9876,
-    colors: true,
-    autoWatch: false,
-    singleRun: true,
-    concurrency: Number.POSITIVE_INFINITY,
-    client: {
-      clearContext: false
-    },
-    files: [
-      {
-        pattern: 'js/tests/unit/**/*.spec.js',
-        watched: false
-      }
-    ],
-    preprocessors: {
-      'js/tests/unit/**/*.spec.js': ['webpack', 'sourcemap']
-    },
-    frameworks: ['jasmine', 'webpack', 'detectBrowsers'],
-    plugins: [
-      'karma-webpack',
-      'karma-jasmine',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-detect-browsers',
-      'karma-coverage',
-      'karma-sourcemap-loader'
-    ],
-    exclude: [],
-    reporters: ['progress', 'coverage'],
-    coverageReporter: {
-      dir: path.resolve(__dirname, 'js/coverage/'),
-      reporters: [
-        {type: 'lcov', subdir: 'report-lcov'},
-        {type: 'text-summary'}
-      ]
-    },
-    detectBrowsers: detectBrowsers,
-    customLaunchers: browsers,
-    browsers: ['Chrome'],
-    webpack: {
-      mode: 'development',
-      cache: true,
-      devtool: 'inline-source-map',
-      module: {
-        rules: [
-          {
-            enforce: 'pre',
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'js/src/'),
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                "plugins": ["istanbul"]
-              }
+const conf = {
+  basePath: '',
+  port: 9876,
+  colors: true,
+  autoWatch: false,
+  singleRun: true,
+  concurrency: Number.POSITIVE_INFINITY,
+  client: {
+    clearContext: false
+  },
+  files: [
+    {
+      pattern: 'js/tests/unit/**/*.spec.js',
+      watched: false
+    }
+  ],
+  preprocessors: {
+    'js/tests/unit/**/*.spec.js': ['webpack', 'sourcemap']
+  },
+  webpack: {
+    mode: 'development',
+    cache: true,
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          include: path.resolve(__dirname, 'js/src/'),
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              "plugins": ["istanbul"]
             }
-          },
-        ],
-      },
+          }
+        },
+      ],
     },
-  })
+  },
+}
+
+if (BROWSERSTACK) {
+  conf.hostname = ip.address()
+  conf.browserStack = {
+    username: ENV.BROWSER_STACK_USERNAME,
+    accessKey: ENV.BROWSER_STACK_ACCESS_KEY,
+    build: `materialstyle-${ENV.GITHUB_SHA ? ENV.GITHUB_SHA.slice(0, 7) + '-' : ''}${new Date().toISOString()}`,
+    project: 'Material Style',
+    retryLimit: 2
+  }
+  plugins.push('karma-browserstack-launcher', 'karma-jasmine-html-reporter')
+  conf.customLaunchers = browsers
+  conf.browsers = Object.keys(browsers)
+  reporters.push('BrowserStack', 'kjhtml')
+} else {
+  frameworks.push('detectBrowsers')
+  plugins.push(
+    'karma-chrome-launcher',
+    'karma-firefox-launcher',
+    'karma-detect-browsers',
+    'karma-coverage'
+  )
+  reporters.push('coverage')
+  conf.detectBrowsers = detectBrowsers
+  conf.coverageReporter = {
+    dir: path.resolve(__dirname, 'js/coverage/'),
+    reporters: [
+      {type: 'lcov', subdir: 'report-lcov'},
+      {type: 'text-summary'}
+    ]
+  }
+
+  if (DEBUG) {
+    conf.hostname = ip.address()
+    plugins.push('karma-jasmine-html-reporter')
+    reporters.push('kjhtml')
+    conf.singleRun = false
+    conf.autoWatch = true
+  }
+}
+
+conf.frameworks = frameworks
+conf.plugins = plugins
+conf.reporters = reporters
+
+module.exports = karmaConfig => {
+  conf.logLevel = karmaConfig.LOG_ERROR
+  karmaConfig.set(conf)
 }
