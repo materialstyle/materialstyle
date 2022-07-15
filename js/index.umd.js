f70b91c9397dea73788976d256c0cc5a49db8d02
Neeraj Kumar Das <nkdas91@gmail.com>
2022-07-15 19:47:10 +0530

    Add esm and umd versions of index js

---
+++ js/index.umd.js
@@ -0,0 +1,50 @@
+/**
+ * --------------------------------------------------------------------------
+ * Material Style (v3.0.0-alpha1): material-style.js
+ * Licensed under MIT (https://github.com/materialstyle/materialstyle/blob/master/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
+import Alert from './src/alert'
+import Button from './src/button'
+import Carousel from './src/carousel'
+import Collapse from './src/collapse'
+import Dropdown from './src/dropdown'
+import Modal from './src/modal'
+import Navbar from './src/navbar'
+import Offcanvas from './src/offcanvas'
+import Popover from './src/popover'
+import Rainbow from './src/rainbow'
+import Ripple from './src/ripple'
+import Scrollspy from './src/scrollspy'
+import SelectField from './src/select-field'
+import Shape from './src/shape'
+import Snackbar from './src/snackbar'
+import Tab from './src/tab'
+import MaterialTab from './src/material-tab'
+import TextField from './src/text-field'
+import Toast from './src/toast'
+import Tooltip from './src/tooltip'
+
+export default {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Navbar,
+  Offcanvas,
+  Popover,
+  Rainbow,
+  Ripple,
+  Scrollspy,
+  SelectField,
+  Shape,
+  Snackbar,
+  Tab,
+  MaterialTab,
+  TextField,
+  Toast,
+  Tooltip
+}
