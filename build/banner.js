bc4a1bbf8c5be7d0f1556b9abc53326d479a3e0b
Neeraj Kumar Das <nkdas91@gmail.com>
2022-07-16 19:07:42 +0530

    Update banner

--- build/banner.js
+++ build/banner.js
@@ -8,10 +8,10 @@ function getBanner(pluginFilename) {
   * Material Style${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
   * Copyright 2018-${year} ${pkg.author}
   * Licensed under MIT (https://github.com/materialstyle/materialstyle/blob/master/LICENSE)  
-  *
   * This a fork of Bootstrap: Initial license below
-  * Bootstrap${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
-  * Copyright 2011-${year} ${pkg.author}
+  * 
+  * Bootstrap${pluginFilename ? ` ${pluginFilename}` : ''} v5.2.0-beta1 (https://getbootstrap.com/)
+  * Copyright 2011-${year} The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
   */`
 }
