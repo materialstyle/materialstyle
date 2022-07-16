5783f137cd5520a0b90f7b68858260d7496d500b
Neeraj Kumar Das <nkdas91@gmail.com>
2022-07-16 14:04:10 +0530

    Add build related files

---
+++ build/banner.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const pkg = require('../package.json')
+const year = new Date().getFullYear()
+
+function getBanner(pluginFilename) {
+  return `/*!
+  * Material Style${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
+  * Copyright 2018-${year} ${pkg.author}
+  * Licensed under MIT (https://github.com/materialstyle/materialstyle/blob/master/LICENSE)  
+  *
+  * This a fork of Bootstrap: Initial license below
+  * Bootstrap${pluginFilename ? ` ${pluginFilename}` : ''} v${pkg.version} (${pkg.homepage})
+  * Copyright 2011-${year} ${pkg.author}
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+  */`
+}
+
+module.exports = getBanner
